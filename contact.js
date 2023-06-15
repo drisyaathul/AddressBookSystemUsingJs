2e3d213b4745fff425dce2b5747226f77fefb411
drisyaathul <drisyak809@gmail.com>
2023-06-16 03:46:44 +0800

    Added: Create AddressBook Contact

---
+++ contact.js
@@ -0,0 +1,13 @@
+//UC1:
+class Contact {
+    constructor (firstName, lastName, address, city, state, zip, phoneNumber, email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+}
